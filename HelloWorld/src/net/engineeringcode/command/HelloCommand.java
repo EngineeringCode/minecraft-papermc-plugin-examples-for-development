78d5eb8c49f23f23ac9883a8bb775c61b273168a
Mart <abc@mart.com>
2020-08-17 13:49:18 +0900

    1. 이클립스 프로젝트에 적합하도록 gitignore 수정 2. Hello World 예제 작성

---
+++ HelloWorld/src/net/engineeringcode/command/HelloCommand.java
@@ -0,0 +1,37 @@
+package net.engineeringcode.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
+
+import net.engineeringcode.mc.HelloWorld;
+
+public class HelloCommand implements CommandExecutor{
+
+		private HelloWorld plugin;
+		
+		public HelloCommand(HelloWorld plugin) {
+			this.plugin = plugin;
+			plugin.getCommand("hi").setExecutor(this);
+			plugin.getCommand("hello").setExecutor(this);
+			plugin.getCommand("helloworld.hello").setExecutor(this);
+		}
+
+		@Override
+		public boolean onCommand(@Nullable CommandSender sender, @Nullable Command command, @Nullable String label, @Nullable String[] args) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("플레이어만 이 명령어를 실행할 수 있습니다.");
+				return true;
+			}
+			Player player = (Player) sender;
+			if(player.hasPermission("helloworld.hello")) {
+				player.sendMessage("안녕하세요!");
+				return true;
+			}else {
+				player.sendMessage("이 명령어에 대한 권한을 가지고 있지 않습니다.");
+			}
+			return false;
+		}
+}
---
+++ HelloWorld/src/net/engineeringcode/mc/HelloWorld.java
@@ -0,0 +1,65 @@
+package net.engineeringcode.mc;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.engineeringcode.command.HelloCommand;
+
+public class HelloWorld extends JavaPlugin {
+	Logger logger = getServer().getLogger();
+	Timer timer = new Timer();
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		logger.info("onEnable() Example Plugins");
+		new HelloCommand(this);
+		
+		timer.schedule(new TimerTask() {			
+			@Override
+			public void run() {
+				sendAnnouncement();
+			}
+		}, 5000, 30000);
+	}
+	
+	@Override
+	public void onLoad() {
+		super.onLoad();
+		logger.info("onLoad() Example Plugins");
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		logger.info("onDisable() Example Plugins");
+	}
+	
+	public void sendAnnouncement() {
+		Random random = new Random(System.currentTimeMillis());
+		switch (random.nextInt(5)) {
+		case 0:
+			getServer().broadcastMessage("본 서버는 이스트큐브 서버이며 홈페이지 주소는 https://www.eastcube.net/ 입니다.");
+			break;
+		case 1:
+			getServer().broadcastMessage("현재 서버는 PaperMC를 기반으로 구성되었습니다.");
+			break;
+		case 2:
+			getServer().broadcastMessage("본 관리자는 플러그인 개발을 직접 합니다.");
+			break;
+		case 3:
+			getServer().broadcastMessage("이 메시지가 신경쓰인다면 저에게 메일을 보내주세요. 꺼드릴게요.");
+			break;
+		case 4:
+			getServer().broadcastMessage("즐거운 하루 되세요.");
+			break;
+		default:
+			getServer().broadcastMessage("예상 밖의 숫자가 나타났군요.");
+			break;
+		}
+		
+	}
+}
